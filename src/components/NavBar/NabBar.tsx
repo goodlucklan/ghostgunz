d1d128fd88cf24180057da37a2cf4a8e0d916439
unknown <goodluckxdc@gmail.com>
2022-08-31 19:19:05 -0500

    First commit

---
+++ src/components/NavBar/NabBar.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { AppBar, Box, Divider, Drawer, IconButton } from "@mui/material";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { Typography, Toolbar, Button } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import { NavBarProps as Props } from "./NavBar";
+
+import { useNavigate } from "react-router-dom";
+
+const drawerWidth = 240;
+const navItems = ["Home", "Register", "Login", "Shop"];
+
+export const NabBar: React.FC<Props> = (props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  // const navigate = useNavigate();
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        GHOSTGUNZ
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar component="nav" color="secondary">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            GHOSTGUNZ
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item}
+                sx={{ color: "#fff", margin: 2 }}
+                // onClick={() => navigate("/home")}
+              >
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+};
---
+++ src/utils/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+export const Navigation = () => {
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+  );
+};
